be8c79051483cae12f1aa78a763802a03849d906
Socheat Sok <socheatsok78@gmail.com>
2025-06-09 21:41:37 +0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs:
+    let
+      forSystem =
+        s: f:
+        inputs.nixpkgs.lib.genAttrs s (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              # Allow unfree packages like Terraform
+              # config.allowUnfree = true;
+            };
+          }
+        );
+
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      forEachSupportedSystem = forSystem supportedSystems;
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+              delve
+            ];
+          };
+        }
+      );
+    };
+}
